b54e152274a62251b88cb780b4f62250ae7a6a7e
Vladimri Jotov <v.jotov@gmail.com>
2020-02-15 16:49:58 +0700

    adding the initial set of files

---
+++ src/main/java/com/company/Claculator.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Claculator {
+    public Result divide(int dividend, int divisor ){
+        return null;
+    }
+}
--- src/test/java/com/company/LongDivisionTest.java
+++ src/test/java/com/company/LongDivisionTest.java
@@ -10,29 +10,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LongDivisionTest {
 
-    @Test
-    void nullValueTest() {
-        assertThrows( )
-        assertEquals(Anagram.process(null),"");
-    }
 
-    @Test
-    void oneWordTest() {
-        assertEquals(Anagram.process("asdf"),"fdsa");
-    }
-
-    @Test
-    void twoWordsTest() {
-        assertEquals(Anagram.process("asdf zxcvb"),"fdsa bvcxz");
-    }
-
-    @Test
-    void twoWordsWithCharactersTest() {
-        assertEquals(Anagram.process("a@s#df 1zx3cvb"),"f@d#sa 1bv3cxz");
-    }
-
-    @Test
-    void numbersTest() {
-        assertEquals(Anagram.process("192837"),"192837");
-    }
 }
